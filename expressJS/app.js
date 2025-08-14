f7486462b509f3385d0af5ffabdcf1de2ad6863a
rafay45 <rafayy579@gmail.com>
2025-08-14 14:19:58 +0500

    🚂 ExpressJS

--- expressJS/app.js
+++ expressJS/app.js
@@ -1,4 +1,32 @@
 const express = require('express')
-
 const app = express()
+app.use(express.json())
 
+// app.get('/', (req, res) => {
+//     res.send([
+//         {
+//       "id": 1,
+//       "Name": "Rafay",
+//       "Father Name": "Faheem",
+//       "Course Name": "Web & mobile app development"
+//     },
+//         {
+//       "id": 2,
+//       "Name": "Wasay",
+//       "Father Name": "Faheem",
+//       "Course Name": "Web & mobile app development"
+//     },
+//         {
+//       "id": 3,
+//       "Name": "sahiban",
+//       "Father Name": "Faheem",
+//       "Course Name": "Web & mobile app development"
+//     },
+// ])
+// })
+
+
+app.listen(3000, () => {
+    console.log('Server is runing at 3000');
+    
+})
